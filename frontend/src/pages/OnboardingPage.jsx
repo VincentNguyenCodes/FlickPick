aeb07aeb0534ae977f698c6465029180caddd520
VincentNguyenCodes <vincentnguyen0535@gmail.com>
2026-03-23 14:45:00 -0700

    import useRef for center card scroll reset

--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Logo from '../components/Logo';
 import StarRating from '../components/StarRating';
