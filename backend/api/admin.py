fe96dd205a9e232bb39ce3db8e6262f706f3ce02
VincentNguyenCodes <vincentnguyen0535@gmail.com>
2026-03-23 14:09:47 -0700

    register Movie model in admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Movie, Rating, UserProfile
 
-# Register your models here.
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'year', 'genre', 'avg_rating')
+    list_filter = ('genre',)
+    search_fields = ('title', 'director')
