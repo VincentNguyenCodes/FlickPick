4743f84478961828189f0bae471150bfb282d629
VincentNguyenCodes <vincentnguyen0535@gmail.com>
2026-03-23 14:23:17 -0700

    add tests for GENRES constant and affinity matrix shape

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .ml_model import (
+    GENRES, NUM_GENRES, GENRE_AFFINITY, EMBEDDING_DIM,
+    USER_FEATURE_DIM, MOVIE_FEATURE_DIM, decade_norm, movie_to_vector,
+)
 
-# Create your tests here.
+
+class GenreConstantsTest(TestCase):
+    def test_genres_count(self):
+        self.assertEqual(len(GENRES), 12)
+        self.assertEqual(NUM_GENRES, 12)
+
+    def test_genre_affinity_shape(self):
+        for genre, row in GENRE_AFFINITY.items():
+            self.assertEqual(len(row), 12, f'{genre} affinity row should have 12 values')
+
+    def test_genre_affinity_self_similarity(self):
+        for i, genre in enumerate(GENRES):
+            self.assertEqual(GENRE_AFFINITY[genre][i], 1.0, f'{genre} self-similarity should be 1.0')
