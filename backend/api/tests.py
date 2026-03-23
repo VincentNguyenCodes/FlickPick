5ba0064acfb93cf9962f0edb629fb0413ee4505e
VincentNguyenCodes <vincentnguyen0535@gmail.com>
2026-03-23 14:23:37 -0700

    add tests for decade_norm and feature dimensions

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -17,3 +17,20 @@ class GenreConstantsTest(TestCase):
     def test_genre_affinity_self_similarity(self):
         for i, genre in enumerate(GENRES):
             self.assertEqual(GENRE_AFFINITY[genre][i], 1.0, f'{genre} self-similarity should be 1.0')
+
+
+class FeatureEngineeringTest(TestCase):
+    def test_decade_norm_range(self):
+        self.assertEqual(decade_norm(1920), 0.0)
+        self.assertEqual(decade_norm(2020), 1.0)
+        self.assertAlmostEqual(decade_norm(1970), 0.5)
+
+    def test_decade_norm_clamp(self):
+        self.assertEqual(decade_norm(1800), 0.0)
+        self.assertEqual(decade_norm(2100), 1.0)
+
+    def test_user_feature_dim(self):
+        self.assertEqual(USER_FEATURE_DIM, 42)
+
+    def test_movie_feature_dim(self):
+        self.assertEqual(MOVIE_FEATURE_DIM, 14)
